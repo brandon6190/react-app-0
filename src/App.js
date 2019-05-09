8640316cf379ab60f65c5553797a220ec7a630ed
brandon6190 <brandon6190@outlook.com>
2019-05-08 21:09:45 -0400

    cleaned code for eslint; preparing for build process

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-const Pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h2', {}, props.breed)
+/* globals React ReactDOM */
+
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
   ]);
-}
+};
 
 class App extends React.Component {
-
   handleTitleClick() {
-    alert('Hello Wolrd!');
+    alert("Hello Wolrd!");
   }
-  
+
   render() {
-    return React.createElement('div', {}, [
-      React.createElement('h1', { onClick: this.handleTitleClick }, 'BF Finder!'),
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "BF Finder!"
+      ),
       React.createElement(Pet, {
-        'name': 'Piru',
-        'animal': 'Dog',
-        'breed': 'Pitbull'
+        name: "Piru",
+        animal: "Dog",
+        breed: "Pitbull"
       }),
       React.createElement(Pet, {
-        'name': 'Onyx',
-        'animal': 'Cat',
-        'breed': 'Bombay'
+        name: "Onyx",
+        animal: "Cat",
+        breed: "Bombay"
       }),
       React.createElement(Pet, {
-        'name': 'Lui',
-        'animal': 'Snake',
-        'breed': 'Python'
-      }),
-    ]);  
+        name: "Lui",
+        animal: "Snake",
+        breed: "Python"
+      })
+    ]);
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));

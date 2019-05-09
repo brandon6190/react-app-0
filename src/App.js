627f9f637be3299bf04324fdb0a171b16aa27d33
brandon6190 <brandon6190@outlook.com>
2019-05-09 19:51:31 -0400

    importing Pet component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
-/* globals React ReactDOM */
+import { Component, createElement } from 'react';
+import { render } from 'react-dom';
+import { Pet } from './Pet';
 
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
-
-class App extends React.Component {
+class App extends Component {
   handleTitleClick() {
     alert("Hello Wolrd!");
   }
 
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
+    return createElement("div", {}, [
+      createElement(
         "h1",
         { onClick: this.handleTitleClick },
         "BF Finder!"
       ),
-      React.createElement(Pet, {
+      createElement(Pet, {
         name: "Piru",
         animal: "Dog",
         breed: "Pitbull"
       }),
-      React.createElement(Pet, {
+      createElement(Pet, {
         name: "Onyx",
         animal: "Cat",
         breed: "Bombay"
       }),
-      React.createElement(Pet, {
+      createElement(Pet, {
         name: "Lui",
         animal: "Snake",
         breed: "Python"
@@ -39,4 +33,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(createElement(App), document.getElementById("root"));

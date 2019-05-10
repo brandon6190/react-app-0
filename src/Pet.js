12804bd1db112cb42e85f509f1df82e74a1c3e7a
brandon6190 <brandon6190@outlook.com>
2019-05-10 16:17:31 -0400

    refactored Pet component to JSX syntax

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,13 @@
-import { createElement } from 'react';
+import React from 'react';
 
 const Pet = props => {
-  return createElement("div", {}, [
-    createElement("h1", {}, props.name),
-    createElement("h2", {}, props.animal),
-    createElement("h2", {}, props.breed)
-  ]);
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
 };
 
 export default Pet;

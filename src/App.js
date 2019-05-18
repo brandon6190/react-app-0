23fed8621cf205986cf209526351b869e4f2c09e
brandon6190 <brandon6190@outlook.com>
2019-05-18 19:18:50 -0400

    refactored: -removed curly braces from line 1 to import React -Added React to line 5, when extending Component to App -removed click handler from h1 on line 13

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import Pet from './Pet';
 
-class App extends Component {
+class App extends React.Component {
   handleTitleClick() {
     alert("Hello Wolrd!");
   }
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1 onCLick={this.handleTitleClick}>BF Finder!</h1>
+        <h1>BF Finder!</h1>
         <Pet name="Piru" animal="Dog" breed="Pitbull"/>
         <Pet name="Onyx" animal="Cat" breed="Bombay"/>
         <Pet name="Lui" animal="Snake" breed="Python"/>

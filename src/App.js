88bc4a1f384f81c1a5bf9c1ee2e3ed6f2ad70a56
brandon6190 <brandon6190@outlook.com>
2019-05-29 01:45:03 -0400

    -imported the perfinder-client api on line 3. -line 6 through line 7, invoking the petfinder-client with API_KEY and API_SECRET in order to interact with the petfinder webservice. -added constructor to the App component and initialized state object with "pets" as an empty array. -when the component mounts find a full list of pets within the New York, NY location with the perfinder api. -check if the desired data exist; then set the state with the data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import { render } from 'react-dom';
+import pf from 'petfinder-client';
 import Pet from './Pet';
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
-  handleTitleClick() {
-    alert("Hello Wolrd!");
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    }
+  }
+
+  componentDidMount() {
+    petfinder.pet.find( { output: 'full', location: 'New York, NY'} )
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) { // is their data in pets and pet from the api
+          if (Array.isArray(data.petfinder.pets.pet)) { // if the api sends back multiple pets
+            pets = data.petfinder.pets.pet; // assign that array to the pets variable
+          }
+          else { // else the api will send back only 1 pet then...
+            pets = [data.petfinder.pets.pet]; // wrap that one pet with an array and assign it to pets
+          }
+        }
+        else { // if pets and pet are null then... 
+          pets = [];
+        }
+
+        this.setState({
+          pets
+        });
+      });
+
   }
 
   render() {

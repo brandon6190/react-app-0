cf9d450fa257657c853f797516b2f595a1bd9e57
brandon6190 <brandon6190@outlook.com>
2019-07-12 11:26:46 -0400

    Fixed formatting, and added to two props to the Pet component

--- src/App.js
+++ src/App.js
@@ -4,71 +4,69 @@ import pf from 'petfinder-client';
 import Pet from './Pet';
 
 const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
+	key: process.env.API_KEY,
+	secret: process.env.API_SECRET,
 });
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      pets: []
-    }
-  }
+		this.state = {
+			pets: [],
+		};
+	}
 
-  componentDidMount() {
-    petfinder.pet.find( { output: 'full', location: 'New York, NY'} )
-      .then(data => {
-        let pets;
+	componentDidMount() {
+		petfinder.pet
+			.find({ output: 'full', location: 'New York, NY' })
+			.then(data => {
+				let pets;
 
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          if (Array.isArray(data.petfinder.pets.pet)) { 
-            pets = data.petfinder.pets.pet; 
-          }
-          else {
-            pets = [data.petfinder.pets.pet];
-          }
-        }
-        else { 
-          pets = [];
-        }
+				if (data.petfinder.pets && data.petfinder.pets.pet) {
+					if (Array.isArray(data.petfinder.pets.pet)) {
+						pets = data.petfinder.pets.pet;
+					} else {
+						pets = [data.petfinder.pets.pet];
+					}
+				} else {
+					pets = [];
+				}
 
-        this.setState({
-          pets
-        });
-      });
+				this.setState({
+					pets,
+				});
+			});
+	}
 
-  }
+	render() {
+		return (
+			<div>
+				<h1>Find A Pet!</h1>
+				<div>
+					{this.state.pets.map(pet => {
+						let breed;
 
-  render() {
-    return (
-      <div>
-        <h1>Find A Pet!</h1>
-        <div>
-          { this.state.pets.map(pet => {
-            let breed;
-
-            if (Array.isArray(pet.breeds.breed)) {
-              breed = pet.breeds.breed.join(', ');
-            }
-            else {
-              breed = pet.breeds.breed;
-            }
-
-            return (
-              <Pet
-                key={pet.id}
-                name={pet.name}
-                animal={pet.animal}
+						if (Array.isArray(pet.breeds.breed)) {
+							breed = pet.breeds.breed.join(', ');
+						} else {
+							breed = pet.breeds.breed;
+						}
+						return (
+							<Pet
+								key={pet.id}
+								name={pet.name}
+								animal={pet.animal}
                 breed={breed}
-              />
-            )
-          }) }
-        </div>
-      </div>
-    )
-  }
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		);
+	}
 }
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));

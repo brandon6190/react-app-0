6df306090c740962d0b615ead2ab95f6c51dea22
brandon6190 <brandon6190@outlook.com>
2019-09-30 12:16:24 -0400

    -Imported ANIMALS array of string from petfineder-client. -Created 'handleAnimalChange' that will set the animal in the set with the events target value. -Created a new label for Animal, and a select element to choose an animal. ANIMALS is being mapped and retuning an option element with the current animal for the value and display name. Change handlers are for the select element

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { ANIMALS } from 'petfinder-client';
 
 class SearchParams extends React.Component {
 	state = {
 		location: 'New York, NY',
 		animal: '',
 		breed: '',
-  };
-  
-  handleLocationChange = event => {
-    this.setState({
-      location: event.target.value,
-    });
-  }
+	};
+
+	handleLocationChange = event => {
+		this.setState({
+			location: event.target.value,
+		});
+	};
+
+	handleAnimalChange = event => {
+		this.setState({
+			animal: event.target.value,
+		});
+	};
 
 	render() {
 		return (
@@ -19,11 +26,28 @@ class SearchParams extends React.Component {
 				<label htmlFor="location">
 					Location:
 					<input
-            onChange={this.handleLocationChange}
+						onChange={this.handleLocationChange}
 						id="location"
 						value={this.state.location}
 						placeholder="Location"
 					/>
+				</label>
+				<label htmlFor="animal">
+					Animal:
+					<select
+						id="animal"
+						value={this.state.animal}
+						onChange={this.handleAnimalChange}
+						onBlur={this.handleAnimalChange}>
+						<option />
+						{ANIMALS.map(animal => {
+							return (
+								<option key={animal} value={animal}>
+									{animal}
+								</option>
+							);
+						})}
+					</select>
 				</label>
 			</div>
 		);

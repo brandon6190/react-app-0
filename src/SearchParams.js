3f56f7c2b30c3e3303f943afc6e4825082cfedb1
brandon6190 <brandon6190@outlook.com>
2019-10-12 18:34:09 -0400

    Created SearchParams Route Component. Rendering the SearchBox Component

---
+++ src/SearchParams.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SearchBox from './SearchBox';
+
+class Search extends Component {
+  render() {
+    return(
+      <SearchBox />
+    )
+  }
+}
+
+export default Search;
